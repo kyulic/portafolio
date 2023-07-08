b1aa703a9ca124971a7f9aac640f44bf7076be7b
kyulic <kycaquimbo@hotmail.com>
2023-07-07 21:59:14 -0500

    6

--- src/components/HeaderPor.jsx
+++ src/components/HeaderPor.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react"
 
 
 
 
 const HeaderPor = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const hanbleClick=()=>setIsOpen(!isOpen)
   return (
     <article>
         <div className="port_init">
@@ -10,19 +13,26 @@ const HeaderPor = () => {
             
             
             <nav className="navbar">
-                <ul className="list_navbar">
+               <div className="nav_logo"></div>
+                <ul className={`list_navbar ${isOpen && "open"}`}>
                     <li><a href="#main">Home</a></li>
                     <li><a href="#about_me">About me</a></li>
-                    <li><a href="#">Portafolio</a></li>
+                    <li><a href="#portafolio">Portafolio</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
+                <div className={`nav_toggle ${isOpen && "open"}`} onClick={hanbleClick}>
+                  <spa><i className='bx bx-list-ul'></i></spa>
+                  <spa></spa>
+                  <spa></spa>
+                </div>
             </nav>
+           
         </header>
         <div className="div_img"> <img className="img_logo" src="../logo.png" alt="logo" /></div>
         <main id="main">
             <div className="inf_content">
-                <h1 className="presentation">Hi!!! I'm <span className="name">Karla Yulieth </span>Caquimbo </h1>
-                <p>A fronted developer based in Colombia</p>
+                <h1 className="presentation">Hola!!! Mi nombre es <span className="name">Karla Yulieth </span>Caquimbo </h1>
+                <p>Soy desarrolladora fronted y backend  | JavaScript | ReactJS | CSS | HTML | PYTHON </p>
                 <button className="button_content">Download CV</button>            
             </div>
             <img className="img_header" src="../karla.jpg" alt="karla" />
@@ -32,7 +42,7 @@ const HeaderPor = () => {
       <div id="about_me">
       <img className="img_ing" src="../ingenieria.png" alt="ingenieria" />
         <div className="about_inf">
-        <h2 className="title_about">About me</h2>
+        <h2 className="title_about">Acerca de mi</h2>
         
         <p className="paraf_about">I am electronic engineer, 
           also developer full-stack developer,
@@ -40,10 +50,26 @@ const HeaderPor = () => {
 
 
         </div>
+        </div>
+
+        <section id="portafolio">
+        <h2 className="title_portafolio">My Portafolio</h2>
+          <p className="parra_portafolio">The following are some of the repositories 
+            that have been developed</p>
+            <h3 className="project_section">Project Description</h3>
+            <ul>
+              <li> <a href="https://kyclima.netlify.app"></a> 
+              
+              <i class="fa-brands fa-github"></i>
+              </li>
+              <li> <a href=""></a> </li>
+              <li> <a href=""></a> </li>
+              <li> <a href=""></a> </li>
+            </ul>
+
+
+        </section>
         
-
-
-      </div>
       <a className="repositorio" href="https://github.com/kyulic/portafolio">Karla Yulieth caquimbo Repositorio</a>
 
       
